a11c300b741c8297acaf2f20c2c3f9625cd67207
suzie6087 <41510235+suzie6087@users.noreply.github.com>
2019-10-06 15:05:15 +0900

    Vue.js Study - make TODO List day1
    TodoInput, TodoLIst

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,28 +1,61 @@
 <!--할 일 입력 및 추가하는 컴포넌트  -->
 <template>
-    <div>
-        <input type="text" v-model="newTodoitem" /> <!-- 데이터의 양반향 소통이 있어야함으로 v-model을 써서 바인딩 해준다. -->
-        <button @click="addTodo" >추가</button> <!--버튼을 클릭했을때 이벤트를 주는 함수를 넣는다. v-on=>@ 대체 가능  -->
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" /> <!-- 데이터의 양반향 소통이 있어야함으로 v-model을 써서 바인딩 해준다. -->
+        <!-- <button @click="addTodo" >추가</button> --><!--버튼을 클릭했을때 이벤트를 주는 함수를 넣는다. v-on=>@ 대체 가능  -->
+        <span class="addContainer" @click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
-    /* 양방향으로 통신이 가능하다. 인풋에 입력을 하면,  newTodoItem에 값이 들어간다. 
-    data() {
-           return {
-               newTodoItem:"" 
-           }
-       },
-       */
+    /* 양방향으로 통신이 가능하다. 인풋에 입력을 하면,  newTodoItem에 값이 들어간다. */
+      data:function() {
+        return {
+            newTodoItem: ""
+        }
+    },
       methods: {
-          addTodo() {
-              console.log(this.newTodoitem); // 클릭 이벤트로 인풋에 담긴 newTodoitem이 콘솔에 찍힌다. 
+          addTodo:function() {
+             // console.log(this.newTodoitem); // 클릭 이벤트로 인풋에 담긴 newTodoitem이 콘솔에 찍힌다. 
+            if(this.newTodoItem !== ''){ // newTodoItem이 비어있지 않다면 
+                  localStorage.setItem(this.newTodoItem, this.newTodoItem);  // localStorage에 저장한다. 정보들을 임시저장한다 크롬 개발자도구에서 확인가능하다. 
+                  this.clearInput(); // 정보를 localStorage에 저장하고 input창을 비워준다.
+            } 
+            
+          },
+          clearInput:function(){
+              this.newTodoItem = ''; // 입력창 비워주기 
           }
       },
     }
 </script>
 
-<style>
-
+<style scoped>
+ input{
+     outline: none;
+ }
+ .inputBox{
+     background: white;
+     height: 50px;
+     line-height: 50px;
+     border-radius: 5px;
+ }
+ .inputBox input{
+     border-style: none;
+     font-size: 0.9rem;
+ }
+ .addContainer{
+     float: right;
+     background: linear-gradient(to right, #6478FB, #8763FB);
+     display: block;
+     width: 3rem;
+     border-radius: 0 5px 5px 0;
+ }
+ .addBtn{
+     color: white;
+     vertical-align: middle;
+ }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,64 @@
 <!--할 일 목룍 표시, 특정 할 일 삭제하는 컴포넌트 -->
 <template>
-    <div>List</div>
+    <div>
+        <ul>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem"> 
+                <!--v-bind:key="todoItem": 만약에 반복문에 바인딩 된 애들이 
+                값이 변경이 되었을때, 반복문 전체를 그리지 않고 변경된 놈만 새로 그려주기위한 key를 주는 것이다. 
+            -->
+                {{todoItem}}
+
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        
+        data:function() {
+            return {
+                todoItems:[] // todo값들을 담을 배열 생성 
+            }
+        },
+        /* life 사이클 method   */
+        /* localStorage에 담긴 값들을 뷰에 출력   */
+        /* Created() 라이프 사이클 메서드에 for문과 push()로 로컬스토리지의 모든 데이터를 todoUtems에 저장하는 로직 구현  */
+        created:function(){
+            if(localStorage.length > 0 ){
+                for(var i = 0 ; i < localStorage.length ; i++){
+                    this.todoItems.push(localStorage.key(i)); // 배열에 넣기 
+                }
+            }
+        }
     }
 </script>
 
-<style>
-
+<style scoped>
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li{
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin:0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .removeBtn{
+        margin-left: auto;
+        color: #de4343;
+    }
+    .checkBtnCompleted{
+        color: #b3adad;
+    }
+    .textCompleted{
+        text-decoration:line-through;
+        color:#b3adad; 
+    }
 </style>
